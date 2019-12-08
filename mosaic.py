1839ab98ec545d8c53acd7d44ea7ed8bef96a6b8
Luke Barker <developer.luke.barker3@outlook.com>
2019-12-08 23:01:38 +0000

    optimisation hacks

--- mosaic.py
+++ mosaic.py
@@ -95,12 +95,15 @@ class MosaicGenerator:
 
 		def __get_tile_diff(self, t1, t2, bail_out_value):
 			diff = 0
-			for i in range(len(t1)):
+			i = 0
+			len_t1 = len(t1)
+			while i < len_t1:
 				#diff += (abs(t1[i][0] - t2[i][0]) + abs(t1[i][1] - t2[i][1]) + abs(t1[i][2] - t2[i][2]))
 				diff += ((t1[i][0] - t2[i][0])**2 + (t1[i][1] - t2[i][1])**2 + (t1[i][2] - t2[i][2])**2)
 				if diff > bail_out_value:
 					# we know already that this isn't going to be the best fit, so no point continuing with this tile
 					return diff
+				i += 1
 			return diff
 
 		def get_best_fit_tile(self, img_data):
@@ -114,8 +117,10 @@ class MosaicGenerator:
 				if diff < min_diff:
 					min_diff = diff
 					best_fit_tile_index = tile_index
+					# "optimisation vs quality" hack
+					if min_diff <= 25000:
+						return best_fit_tile_index
 				tile_index += 1
-
 			return best_fit_tile_index
 
 	@staticmethod
